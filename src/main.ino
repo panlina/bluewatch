d88855316fdb67f3702e4a3a24cc1e2ce1be3131
Jack Lu <jacklu@jacklu.me>
2024-02-25 18:14:16 +0800

    +light sleep

--- src/main.ino
+++ src/main.ino
@@ -1,6 +1,8 @@
 #include <LilyGoLib.h>
 #include <LV_Helper.h>
 
+const uint32_t screenTimeout = 10000;
+
 void setup()
 {
 	Serial.begin(115200);
@@ -15,5 +17,25 @@ void setup()
 void loop()
 {
 	lv_task_handler();
+	if (lv_disp_get_inactive_time(NULL) >= screenTimeout)
+		enterLightSleep();
 	delay(5);
+}
+
+void enterLightSleep()
+{
+	Serial.println("Enter light sleep mode.");
+
+	auto brightness = watch.getBrightness();
+	watch.decrementBrightness(0);
+
+	esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+	gpio_wakeup_enable((gpio_num_t)BOARD_TOUCH_INT, GPIO_INTR_LOW_LEVEL);
+	esp_sleep_enable_gpio_wakeup();
+
+	esp_light_sleep_start();
+
+	lv_disp_trig_activity(NULL);
+
+	watch.incrementalBrightness(brightness);
 }
