1c0ed6f2ad457899154cf3f2a6a1248e3ce71f2c
Jack Lu <jacklu@jacklu.me>
2024-04-12 18:03:15 +0800

    websocket: Client.send: -len, +default timeout

--- lib/DuktapeWatch/websocket.cpp
+++ lib/DuktapeWatch/websocket.cpp
@@ -33,9 +33,8 @@ static duk_ret_t js_Client_send(duk_context *ctx) {
 	duk_get_prop_string(ctx, -1, "handle");
 	auto handle = (esp_websocket_client_handle_t)duk_get_pointer(ctx, -1);
 	auto data = duk_get_string(ctx, 0);
-	auto len = duk_get_int(ctx, 1);
-	auto timeout = (TickType_t)duk_get_uint(ctx, 2);
-	auto n = esp_websocket_client_send_text(handle, data, len, timeout);
+	auto timeout = (TickType_t)duk_get_uint_default(ctx, 1, portMAX_DELAY);
+	auto n = esp_websocket_client_send_text(handle, data, strlen(data), timeout);
 	duk_push_int(ctx, n);
 	return 1;
 }
@@ -63,7 +62,7 @@ void push_Client_prototype(duk_context *ctx) {
 	duk_push_object(ctx);
 	duk_push_c_function(ctx, js_Client_start, 0);
 	duk_put_prop_string(ctx, -2, "start");
-	duk_push_c_function(ctx, js_Client_send, 3);
+	duk_push_c_function(ctx, js_Client_send, 2);
 	duk_put_prop_string(ctx, -2, "send");
 	duk_push_c_function(ctx, js_Client_close, 1);
 	duk_put_prop_string(ctx, -2, "close");
