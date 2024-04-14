efe5bcdc8e5799b6f1bac831c3017c42bf87de4b
Jack Lu <jacklu@jacklu.me>
2024-04-14 19:35:07 +0800

    HttpServer: +registerUriHandler

--- lib/DuktapeWatch/HttpServer.cpp
+++ lib/DuktapeWatch/HttpServer.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <duktape.h>
 #include <esp_http_server.h>
+#include <functional>
+
+using std::function;
 
 static duk_ret_t js_HttpServer(duk_context *ctx) {
 	duk_require_constructor_call(ctx);
@@ -16,14 +19,83 @@ static duk_ret_t js_HttpServer(duk_context *ctx) {
 	duk_push_this(ctx);
 	duk_push_pointer(ctx, handle);
 	duk_put_prop_string(ctx, -2, "handle");
+
+	duk_push_array(ctx);
+	duk_put_prop_string(ctx, -2, "handlers");
 	return 0;
 }
 
+/*
+	Duktape `HttpServer`s use `user_ctx` as a general means to maintain handler contexts,
+	eliminating any need to pass context data explicitly.
+
+	Handlers are put to the stash, using the addresses of wrapper `std::function` instances
+	as keys, and are deleted when the server is stopped.
+*/
 static duk_ret_t js_HttpServer_stop(duk_context *ctx) {
 	duk_push_this(ctx);
 	duk_get_prop_string(ctx, -1, "handle");
 	auto handle = (httpd_handle_t)duk_get_pointer(ctx, -1);
+
+	duk_push_this(ctx);
+	duk_get_prop_string(ctx, -1, "handlers");
+
+	auto n = duk_get_length(ctx, -1);
+	duk_push_global_stash(ctx);
+	// [..., handlers, stash]
+	for (duk_size_t i = 0; i < n; i++) {
+		duk_get_prop_index(ctx, -2, i);
+		auto user_ctx = (function<esp_err_t(httpd_req_t *)> *)duk_get_pointer(ctx, -1);
+		delete user_ctx;
+		duk_pop(ctx);
+		char stashKey[32];
+		sprintf(stashKey, "httpd_handler_%x", user_ctx);
+		duk_del_prop_string(ctx, -1, stashKey);
+	}
+
 	httpd_stop(handle);
+	return 0;
+}
+
+static duk_ret_t js_HttpServer_registerUriHandler(duk_context *ctx) {
+	auto uri = duk_get_string(ctx, 0);
+	auto method = (httpd_method_t)duk_get_uint(ctx, 1);
+	duk_push_global_stash(ctx);
+	duk_dup(ctx, 2);
+	auto user_ctx = new function<esp_err_t(httpd_req_t *)>([=](httpd_req_t *req) {
+		auto user_ctx = req->user_ctx;
+		duk_push_global_stash(ctx);
+		char stashKey[32];
+		sprintf(stashKey, "httpd_handler_%x", user_ctx);
+		duk_get_prop_string(ctx, -1, stashKey);
+		duk_push_pointer(ctx, req);
+		duk_call(ctx, 1);
+		duk_pop_2(ctx);
+		return ESP_OK;
+	});
+	char stashKey[32];
+	sprintf(stashKey, "httpd_handler_%x", user_ctx);
+	duk_put_prop_string(ctx, -2, stashKey);
+	duk_push_this(ctx);
+	duk_get_prop_string(ctx, -1, "handlers");
+	duk_push_pointer(ctx, user_ctx);
+	auto length = duk_get_length(ctx, -2);
+	duk_put_prop_index(ctx, -2, length);
+	httpd_uri_t uriHandler = {
+		.uri = uri,
+		.method = method,
+		.handler = [](httpd_req_t *req) {
+			auto user_ctx = (function<esp_err_t(httpd_req_t *)> *)req->user_ctx;
+			return (*user_ctx)(req);
+		},
+		.user_ctx = user_ctx
+	};
+	duk_push_this(ctx);
+	duk_get_prop_string(ctx, -1, "handle");
+	auto handle = (httpd_handle_t)duk_get_pointer(ctx, -1);
+	auto err = httpd_register_uri_handler(handle, &uriHandler);
+	if (err)
+		duk_generic_error(ctx, "http server register uri handler error: %d.", err);
 	return 0;
 }
 
@@ -31,6 +103,8 @@ void push_HttpServer_prototype(duk_context *ctx) {
 	duk_push_object(ctx);
 	duk_push_c_function(ctx, js_HttpServer_stop, 0);
 	duk_put_prop_string(ctx, -2, "stop");
+	duk_push_c_function(ctx, js_HttpServer_registerUriHandler, 3);
+	duk_put_prop_string(ctx, -2, "registerUriHandler");
 }
 
 void duktape_watch_install_HttpServer(duk_context *ctx) {
@@ -38,4 +112,14 @@ void duktape_watch_install_HttpServer(duk_context *ctx) {
 	push_HttpServer_prototype(ctx);
 	duk_put_prop_string(ctx, -2, "prototype");
 	duk_put_global_string(ctx, "HttpServer");
+	duk_push_uint(ctx, HTTP_DELETE);
+	duk_put_global_string(ctx, "HTTP_DELETE");
+	duk_push_uint(ctx, HTTP_GET);
+	duk_put_global_string(ctx, "HTTP_GET");
+	duk_push_uint(ctx, HTTP_HEAD);
+	duk_put_global_string(ctx, "HTTP_HEAD");
+	duk_push_uint(ctx, HTTP_POST);
+	duk_put_global_string(ctx, "HTTP_POST");
+	duk_push_uint(ctx, HTTP_PUT);
+	duk_put_global_string(ctx, "HTTP_PUT");
 }
