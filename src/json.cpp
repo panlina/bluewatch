88030cb1dfc6232f89f8200a61f8229063bf224d
Jack Lu <jacklu@jacklu.me>
2024-05-01 23:34:03 +0800

    json: +array, object

--- src/json.cpp
+++ src/json.cpp
@@ -1,9 +1,56 @@
+#include <vector>
+#include <map>
+#include <initializer_list>
+#include <utility>
 #include "json.h"
+
+using std::vector;
+using std::map;
+using std::initializer_list;
+using std::pair;
+using std::size_t;
 
 Json::Json() {}
 Json::Json(bool value) : type(Json::Type::boolean), value({boolean : value}) {}
 Json::Json(double value) : type(Json::Type::number), value({number : value}) {}
 Json::Json(const char *value) : type(Json::Type::string), value({string : value}) {}
+Json::Json(initializer_list<Json> value) : type(Json::Type::array), value({array : new vector<Json>(value)}) {}
+Json::Json(initializer_list<pair<const char *const, Json>> value) : type(Json::Type::object), value({object : new map<const char *, Json>(value)}) {}
+Json::Json(const Json &that) {
+	type = that.type;
+	switch (type) {
+	case Type::boolean: value.boolean = that.value.boolean; break;
+	case Type::number: value.number = that.value.number; break;
+	case Type::string: value.string = that.value.string; break;
+	case Type::array: value.array = new vector<Json>(*that.value.array); break;
+	case Type::object: value.object = new map<const char *, Json>(*that.value.object); break;
+	}
+}
+Json &Json::operator=(const Json &that) {
+	switch (type) {
+	case Type::array: delete value.array; break;
+	case Type::object: delete value.object; break;
+	}
+	type = that.type;
+	switch (type) {
+	case Type::boolean: value.boolean = that.value.boolean; break;
+	case Type::number: value.number = that.value.number; break;
+	case Type::string: value.string = that.value.string; break;
+	case Type::array: value.array = new vector<Json>(*that.value.array); break;
+	case Type::object: value.object = new map<const char *, Json>(*that.value.object); break;
+	}
+	return *this;
+}
+Json::~Json() {
+	switch (type) {
+	case Type::array: delete value.array; break;
+	case Type::object: delete value.object; break;
+	}
+}
 Json::operator bool() const { return value.boolean; }
 Json::operator double() const { return value.number; }
 Json::operator const char *() const { return value.string; }
+Json &Json::operator[](int index) { return (*value.array)[index]; }
+size_t Json::length() const { return value.array->size(); }
+void Json::push(const Json &element) { value.array->push_back(element); }
+Json &Json::operator[](const char *key) { return (*value.object)[key]; }
--- src/json.h
+++ src/json.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <vector>
+#include <map>
+#include <initializer_list>
+#include <utility>
+
 struct Json {
 	enum Type {
 		undefined,
@@ -15,12 +20,23 @@ struct Json {
 		bool boolean;
 		double number;
 		const char *string;
+		std::vector<Json> *array;
+		std::map<const char *, Json> *object;
 	} value;
 	Json();
 	Json(bool);
 	Json(double);
 	Json(const char *);
+	Json(std::initializer_list<Json>);
+	Json(std::initializer_list<std::pair<const char *const, Json>>);
+	Json(const Json &);
+	Json &operator=(const Json &);
+	~Json();
 	explicit operator bool() const;
 	explicit operator double() const;
 	explicit operator const char *() const;
+	Json &operator[](int);
+	std::size_t length() const;
+	void push(const Json &);
+	Json &operator[](const char *);
 };
