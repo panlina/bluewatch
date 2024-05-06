6a100e933ce81833582dfe8b38632d5c7e8cbfce
Jack Lu <jacklu@jacklu.me>
2024-05-06 23:36:46 +0800

    fs: +exists, remove, rename, listDir

--- lib/DuktapeWatch/fs.cpp
+++ lib/DuktapeWatch/fs.cpp
@@ -24,11 +24,54 @@ static duk_ret_t js_writeFile(duk_context *ctx) {
 	return 1;
 }
 
+static duk_ret_t js_exists(duk_context *ctx) {
+	auto path = duk_get_string(ctx, 0);
+	duk_push_boolean(ctx, SPIFFS.exists(path));
+	return 1;
+}
+
+static duk_ret_t js_remove(duk_context *ctx) {
+	auto path = duk_get_string(ctx, 0);
+	duk_push_boolean(ctx, SPIFFS.remove(path));
+	return 1;
+}
+
+static duk_ret_t js_rename(duk_context *ctx) {
+	auto from = duk_get_string(ctx, 0);
+	auto to = duk_get_string(ctx, 1);
+	duk_push_boolean(ctx, SPIFFS.rename(from, to));
+	return 1;
+}
+
+static duk_ret_t js_listDir(duk_context *ctx) {
+	auto path = duk_get_string(ctx, 0);
+
+	auto root = SPIFFS.open(path);
+	auto file = root.getNextFileName();
+	auto i = 0;
+	duk_push_array(ctx);
+	while (!file.isEmpty()) {
+		duk_push_string(ctx, file.c_str());
+		duk_put_prop_index(ctx, -2, i++);
+		file = root.getNextFileName();
+	}
+
+	return 1;
+}
+
 void duktape_watch_install_fs(duk_context *ctx) {
 	duk_push_object(ctx);
 	duk_push_c_function(ctx, js_readFile, 1);
 	duk_put_prop_string(ctx, -2, "readFile");
 	duk_push_c_function(ctx, js_writeFile, 2);
 	duk_put_prop_string(ctx, -2, "writeFile");
+	duk_push_c_function(ctx, js_exists, 1);
+	duk_put_prop_string(ctx, -2, "exists");
+	duk_push_c_function(ctx, js_remove, 1);
+	duk_put_prop_string(ctx, -2, "remove");
+	duk_push_c_function(ctx, js_rename, 2);
+	duk_put_prop_string(ctx, -2, "rename");
+	duk_push_c_function(ctx, js_listDir, 1);
+	duk_put_prop_string(ctx, -2, "listDir");
 	duk_put_global_string(ctx, "fs");
 }
